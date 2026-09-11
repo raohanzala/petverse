c04b43232c5f556f2151d57fd0dba066f3fda75d
imanrao90 <imanrao90@gmail.com>
2026-09-11 14:07:36 +0500

    fix: resolve attendance tab type error

--- components/schedule/boarding/boarding-tabs.tsx
+++ components/schedule/boarding/boarding-tabs.tsx
@@ -75,7 +75,7 @@ export function BoardingTabs({
 
       <TabsContent value="attendance">
         <AttendanceTab
-          params={params}
+          // params={params}
           searchParams={searchParams}
         />
       </TabsContent>
